cd70df0c86ca6f175a2f25eee32c6c05f1ef1ca3
adiatma85 <adiatma85@gmail.com>
2022-05-05 23:47:23 +0700

    src: add after find hook in shotener url

--- internal/pkg/models/url.go
+++ internal/pkg/models/url.go
@@ -1,8 +1,20 @@
 package models
 
+import (
+	"github.com/adiatma85/golang-rest-template-api/internal/pkg/config"
+	"gorm.io/gorm"
+)
+
 // Struct for Url Models
 type Url struct {
 	Model
 	OriginalUrl string `gorm:"type:varchar(255)" json:"url"`
 	ShortenUrl  string `gorm:"type:varchar(255)" json:"shorten_url"`
+}
+
+// Hook Afterfind to return formatted url in shorten url
+func (u *Url) AfterFind(tx *gorm.DB) (err error) {
+	urlEndpoint := "/api/v1/url/load"
+	u.ShortenUrl = config.GetConfig().Server.Endpoint + urlEndpoint + "/" + u.ShortenUrl
+	return
 }
