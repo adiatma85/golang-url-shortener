a14f8c4409b35ec421a8f7bd300e5e7d4efe25df
adiatma85 <adiatma85@gmail.com>
2022-06-03 21:13:04 +0700

    modify: change UD type from uint64 to uint

--- internal/pkg/models/base.go
+++ internal/pkg/models/base.go
@@ -8,7 +8,7 @@ import (
 
 // Struct that derivative from gorm.Model but customized
 type Model struct {
-	ID        uint64         `gorm:"column:id;primary_key;auto_increment;" json:"id"`
+	ID        uint           `gorm:"column:id;primary_key;auto_increment;" json:"id"`
 	CreatedAt time.Time      `gorm:"column:created_at;type:datetime;not null;" json:"created_at"`
 	UpdatedAt time.Time      `gorm:"column:updated_at;type:datetime;not null;" json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index;column:deleted_at;type:datetime" json:"deleted_at"`
--- internal/pkg/models/user.go
+++ internal/pkg/models/user.go
@@ -8,7 +8,7 @@ type User struct {
 	Name     string `gorm:"type:varchar(100)" json:"name" validation:"name"`
 	Email    string `gorm:"type:varchar(100);unique;" json:"email" validation:"email"`
 	Password string `gorm:"type:varchar(100)" json:"-" validation:"password"`
-	RoleId   uint64 `gorm:"not null" json:"-" validation:"role_id"`
+	RoleId   uint   `gorm:"not null" json:"-" validation:"role_id"`
 	Role     Role   `gorm:"foreignkey:RoleId;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"role"`
 }
 
