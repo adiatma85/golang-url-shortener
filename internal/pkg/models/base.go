eadcdddf30fae072d05e950f2340f52f7b1d2f83
adiatma85 <adiatma85@gmail.com>
2022-06-04 17:43:57 +0700

    src: modify base model to adapt both with mysql and postgres

--- internal/pkg/models/base.go
+++ internal/pkg/models/base.go
@@ -9,7 +9,7 @@ import (
 // Struct that derivative from gorm.Model but customized
 type Model struct {
 	ID        uint           `gorm:"column:id;primary_key;auto_increment;" json:"id"`
-	CreatedAt time.Time      `gorm:"column:created_at;type:datetime;not null;" json:"created_at"`
-	UpdatedAt time.Time      `gorm:"column:updated_at;type:datetime;not null;" json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index;column:deleted_at;type:datetime" json:"deleted_at"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
 }
